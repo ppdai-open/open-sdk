27dd70cb035ccc1b8fc7990d9a875e259144253f
xuzhishen <xuzhishen@ppdai.com>
2017-06-30 13:27:36 +0800

    1

--- Java/src/test/java/com/ppdai/open/core/BizTest.java
+++ Java/src/test/java/com/ppdai/open/core/BizTest.java
@@ -18,13 +18,13 @@ import java.util.UUID;
 public class BizTest {
 
     /************ 应用ＩＤ **************/
-    private String appid = "8cf65377538741c2ba8add2615a22299";
+    private String appid = "";
 
     /***************** 客户端私钥 **************/
-    String privKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAObvCCKGy7XrQStuaVIZUaVoO2SQOjWc5/8oF5bKxgIF2+XaLJAXPImKN1TRnKhnf5hx1h4AAram5e66QQXm9va5x1Msj3+wRWa4H8J1Snx/xcvIvd2jtggTI+UDHE7Isi4r6DmdQIYusWIMciqWv6k1a/wOgrZ9bizZLCvyQyaNAgMBAAECgYEAkZX35uxb2KArQZolw6A6nLCNYOScDdiRWYaI+IN1OSlAbDOTDHPZskv0fgB3ZqN5CT4a4mbXTxcAtfOtsm7dGZN7uuFbmBvMfZNvbdKR1WMg8t6aN2de+pq/nxVjqDyDlq6NgMLinAp5547clpxO6fVy9C3gdu8zNujszcnkw4ECQQD3/UBxHOVUU1HQTrLsZfEwexQ9mLoz+nND1DumX5/bVfKy5tLJ5gdsTdx9lre+wQgD+ny2AgTg3XylOJthhUiZAkEA7mS9N/mSHBJxGsrU+y02fayUNBhJnqKcaHD8SQQtQM+iY8npyPKnhfOuWUWkNYWxDZMLshhSJLfowrAI4LsCFQJBAJq+tjqfHF9dSp0grrei+TkWZE/3gKG0s75srfgojXjxjeui63+/AXzMrTzVEr1+eID7Cl0Mr3yd2QzEYPkekWkCQCcSIaTr5VNuflUKyy9DcLvWLg//rwkt036WjfKVmakWnqHGNOe3RVeB4aBF9qzC9Yqo//C3ZbcY4TAPU9UpMSUCQERrLYYFMdCSkiwWvqPN+Ilh1BSheHDbhQSGiiClzw01809I1gPhT6kRM2vN94ac0vhRrz+bQ/Nf72332ZgUvBM=";
+    String privKey = "";
 
     /***************** 服务端公钥 ***************/
-    String pubKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8iMpEG3mnFlMfufO95DfAfor80RL3I/IzF828aoDDw/Xy86jPiihJyGyG2ZmbqsAw+8nj8eGc+U9LmKASgQhS9e0R/MmYDa9R/O2f4tQZUQr3nE3uUTES0tqCLoE3TVSd59lnVExeDL5IW+F/Yc9mz1v+xSDFcSKyfHEo0FDnnwIDAQAB";
+    String pubKey = "";
 
 
     /*********** 授权信息 ***************/
