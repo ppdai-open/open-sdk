58117085a4a1dff8c3a1d0e6e09c46d1f7688e5e
ppdai-open <yanglei@ppdai.com>
2018-04-02 13:38:27 +0800

    Update http.php
    添加https 支持

--- PHP/core/http.php
+++ PHP/core/http.php
@@ -52,6 +52,7 @@ function SendRequest ( $url, $request, $appId, $accessToken ){
         curl_setopt ( $curl, CURLOPT_POST, 1 );
         curl_setopt ( $curl, CURLOPT_POSTFIELDS, $request );
         curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec ( $curl );
         curl_close ( $curl );
 //         $j = json_decode ( $result, true );
